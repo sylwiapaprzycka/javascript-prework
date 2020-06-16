f9ec240f5ece6522713336c10a9c200cb02d4c4a
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-06-16 20:49:32 +0200

    Add first script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Twoja kolej: ' + playerMove + '! Gratulacje!');
