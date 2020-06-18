7153101f0c13fbad5a092fd0cd47a6adc8774b70
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-06-18 12:19:17 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Twoja kolej: ' + playerMove + '! Gratulacje!');
+
+var playerInput, playerMove, item;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch.';
+  playerMove = String(playerMove) + String(' Czyli: kamień');
+}
+printMessage('Twój ruch: ' + playerMove);
